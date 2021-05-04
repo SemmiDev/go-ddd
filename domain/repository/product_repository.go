c38f5bc396655d7359504497cb16419398273c0f
SemmiDev <sammidev4@gmail.com>
2021-05-04 08:43:56 +0700

    user and product repo

--- domain/repository/product_repository.go
+++ domain/repository/product_repository.go
@@ -1,1 +1,11 @@
 package repository
+
+import "DDD/domain/entity"
+
+type ProductRepository interface {
+	SaveProduct(*entity.Product) (*entity.Product, map[string]string)
+	GetProduct(uint64) (*entity.Product, error)
+	GetAllProduct() ([]entity.Product, error)
+	UpdateProduct(*entity.Product) (*entity.Product, map[string]string)
+	DeleteProduct(uint64) error
+}
--- domain/repository/user_repository.go
+++ domain/repository/user_repository.go
@@ -1,1 +1,10 @@
 package repository
+
+import "DDD/domain/entity"
+
+type UserRepository interface {
+	SaveUser(*entity.User) (*entity.User, map[string]string)
+	GetUser(uint64) (*entity.User, error)
+	GetUsers() ([]entity.User, error)
+	GetUserByEmailAndPassword(*entity.User) (*entity.User, map[string]string)
+}
